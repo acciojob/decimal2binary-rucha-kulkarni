990678af5eda95198e02e9ffd5db879dcb7f5bf4
rucha-kulkarni <118054484+rucha-kulkarni@users.noreply.github.com>
2023-07-24 19:43:13 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,13 +1,6 @@
 function decimalToBinary(num) {
-	let bin = 0;
-	let rem, i=1;
-	while (num != 0) {
-		rem = num % 2;
-		num = parseInt(num/2);
-		bin = bin + rem * i;
-		i = i * 10;
-	}
-  return bin;
+	let binary = num.toString(2);
+  return (binary);
 }
 
 window.decimalToBinary = decimalToBinary;
